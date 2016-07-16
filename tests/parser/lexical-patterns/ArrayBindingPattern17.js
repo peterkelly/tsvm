06089077cb1079954e735b6ac13a7cff6ed44b5c
Peter Kelly <peter@pmkelly.net>
2016-07-16 20:50:02 +0700

    Use separate ArrayBinding node types
    Avoid need for list manipulation when creating nodes during parse

--- tests/parser/lexical-patterns/ArrayBindingPattern17.js
+++ tests/parser/lexical-patterns/ArrayBindingPattern17.js
@@ -8,8 +8,8 @@ Module 0-23 "let [ , , , ] = value ;"
     Let 0-23 "let [ , , , ] = value ;"
       [] 4-21 "[ , , , ] = value"
         LexicalPatternBinding 4-21 "[ , , , ] = value"
-          ArrayBindingPattern 4-13 "[ , , , ]"
-            [] 6-11 ", , ,"
-              Elision(3) 6-11 ", , ,"
+          ArrayBindingPattern1 4-13 "[ , , , ]"
+            Elision(3) 6-11 ", , ,"
+            null
           IdentifierReference("value") 16-21 "value"
 *******************************************************************************/
