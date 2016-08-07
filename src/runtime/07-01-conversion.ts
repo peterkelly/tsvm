36e981affac911905601553d36c01a7375fce389
Peter Kelly <peter@pmkelly.net>
2016-08-07 23:34:10 +0700

    Fill in types for built-in abstract operations

--- src/runtime/07-01-conversion.ts
+++ src/runtime/07-01-conversion.ts
@@ -15,107 +15,123 @@
 // ES6 Section 7.1: Type Conversion
 
 import {
-    UnknownType,
+    ValueType,
+    JSValue,
+    JSPropertyKey,
+    JSUndefined,
+    JSNull,
+    JSBoolean,
+    JSString,
+    JSSymbol,
+    JSNumber,
+    JSInteger,
+    JSInt32,
+    JSUInt32,
+    JSInt16,
+    JSUInt16,
+    JSInt8,
+    JSUInt8,
+    JSObject,
     Completion,
     Realm,
 } from "./datatypes";
 
 // ES6 Section 7.1.1: ToPrimitive (input [, PreferredType])
 
-export function ToPrimitive(realm: Realm, input: UnknownType, preferredType?: UnknownType): Completion<UnknownType> {
+export function ToPrimitive(realm: Realm, input: JSValue, preferredType?: ValueType): Completion<JSValue> {
     throw new Error("ToPrimitive not implemented");
 }
 
-export function OrdinaryToPrimitive(realm: Realm, O: UnknownType, hint: UnknownType): Completion<UnknownType> {
+export function OrdinaryToPrimitive(realm: Realm, O: JSObject, hint: "string" | "number"): Completion<JSValue> {
     throw new Error("OrdinaryToPrimitive not implemented");
 }
 
 // ES6 Section 7.1.2: ToBoolean (argument)
 
-export function ToBoolean(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToBoolean(realm: Realm, argument: JSValue): Completion<JSBoolean> {
     throw new Error("ToBoolean not implemented");
 }
 
 // ES6 Section 7.1.3: ToNumber (argument)
 
-export function ToNumber(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToNumber(realm: Realm, argument: JSValue): Completion<JSNumber> {
     throw new Error("ToNumber not implemented");
 }
 
 // ES6 Section 7.1.4: ToInteger (argument)
 
-export function ToInteger(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToInteger(realm: Realm, argument: JSValue): Completion<JSInteger> {
     throw new Error("ToInteger not implemented");
 }
 
 // ES6 Section 7.1.5: ToInt32 (argument)
 
-export function ToInt32(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToInt32(realm: Realm, argument: JSValue): Completion<JSInt32> {
     throw new Error("ToInt32 not implemented");
 }
 
 // ES6 Section 7.1.6: ToUint32 (argument)
 
-export function ToUint32(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToUint32(realm: Realm, argument: JSValue): Completion<JSUInt32> {
     throw new Error("ToUint32 not implemented");
 }
 
 // ES6 Section 7.1.7: ToInt16 (argument)
 
-export function ToInt16(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToInt16(realm: Realm, argument: JSValue): Completion<JSInt16> {
     throw new Error("ToInt16 not implemented");
 }
 
 // ES6 Section 7.1.8: ToUint16 (argument)
 
-export function ToUint16(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToUint16(realm: Realm, argument: JSValue): Completion<JSUInt16> {
     throw new Error("ToUint16 not implemented");
 }
 
 // ES6 Section 7.1.9: ToInt8 (argument)
 
-export function ToInt8(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToInt8(realm: Realm, argument: JSValue): Completion<JSInt8> {
     throw new Error("ToInt8 not implemented");
 }
 
 // ES6 Section 7.1.10: ToUint8 (argument)
 
-export function ToUint8(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToUint8(realm: Realm, argument: JSValue): Completion<JSUInt8> {
     throw new Error("ToUint8 not implemented");
 }
 
 // ES6 Section 7.1.11: ToUint8Clamp (argument)
 
-export function ToUint8Clamp(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToUint8Clamp(realm: Realm, argument: JSValue): Completion<JSUInt8> {
     throw new Error("ToUint8Clamp not implemented");
 }
 
 // ES6 Section 7.1.12: ToString (argument)
 
-export function ToString(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToString(realm: Realm, argument: JSValue): Completion<JSString> {
     throw new Error("ToString not implemented");
 }
 
 // ES6 Section 7.1.13: ToObject (argument)
 
-export function ToObject(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToObject(realm: Realm, argument: JSValue): Completion<JSObject> {
     throw new Error("ToObject not implemented");
 }
 
 // ES6 Section 7.1.14: ToPropertyKey (argument)
 
-export function ToPropertyKey(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToPropertyKey(realm: Realm, argument: JSValue): Completion<JSPropertyKey> {
     throw new Error("ToPropertyKey not implemented");
 }
 
 // ES6 Section 7.1.15: ToLength (argument)
 
-export function ToLength(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function ToLength(realm: Realm, argument: any): Completion<JSNumber> {
     throw new Error("ToLength not implemented");
 }
 
 // ES6 Section 7.1.16: CanonicalNumericIndexString (argument)
 
-export function CanonicalNumericIndexString(realm: Realm, argument: UnknownType): Completion<UnknownType> {
+export function CanonicalNumericIndexString(realm: Realm, argument: JSString): Completion<JSString | JSUndefined> {
     throw new Error("CanonicalNumericIndexString not implemented");
 }
--- src/runtime/07-03-objects.ts
+++ src/runtime/07-03-objects.ts
@@ -15,139 +15,142 @@
 // ES6 Section 7.3: Operations on Objects
 
 import {
-    UnknownType,
+    JSValue,
+    JSPropertyKey,
+    JSString,
+    JSObject,
     Completion,
     Realm,
 } from "./datatypes";
 
 // ES6 Section 7.3.1: Get (O, P)
 
-export function Get(realm: Realm, O: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function Get(realm: Realm, O: JSObject, P: JSPropertyKey): Completion<JSValue> {
     throw new Error("Get not implemented");
 }
 
 // ES6 Section 7.3.2: GetV (V, P)
 
-export function GetV(realm: Realm, V: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function GetV(realm: Realm, V: JSValue, P: JSPropertyKey): Completion<JSValue> {
     throw new Error("GetV not implemented");
 }
 
 // ES6 Section 7.3.3: Set (O, P, V, Throw)
 
-export function Set(realm: Realm, O: UnknownType, P: UnknownType, V: UnknownType, Throw: UnknownType): Completion<UnknownType> {
+export function Set(realm: Realm, O: JSObject, P: JSPropertyKey, V: JSValue, Throw: boolean): Completion<boolean> {
     throw new Error("Set not implemented");
 }
 
 // ES6 Section 7.3.4: CreateDataProperty (O, P, V)
 
-export function CreateDataProperty(realm: Realm, O: UnknownType, P: UnknownType, V: UnknownType): Completion<UnknownType> {
+export function CreateDataProperty(realm: Realm, O: JSObject, P: JSPropertyKey, V: JSValue): Completion<boolean> {
     throw new Error("CreateDataProperty not implemented");
 }
 
 // ES6 Section 7.3.5: CreateMethodProperty (O, P, V)
 
-export function CreateMethodProperty(realm: Realm, O: UnknownType, P: UnknownType, V: UnknownType): Completion<UnknownType> {
+export function CreateMethodProperty(realm: Realm, O: JSObject, P: JSPropertyKey, V: JSValue): Completion<boolean> {
     throw new Error("CreateMethodProperty not implemented");
 }
 
 // ES6 Section 7.3.6: CreateDataPropertyOrThrow (O, P, V)
 
-export function CreateDataPropertyOrThrow(realm: Realm, O: UnknownType, P: UnknownType, V: UnknownType): Completion<UnknownType> {
+export function CreateDataPropertyOrThrow(realm: Realm, O: JSObject, P: JSPropertyKey, V: JSValue): Completion<boolean> {
     throw new Error("CreateDataPropertyOrThrow not implemented");
 }
 
 // ES6 Section 7.3.7: DefinePropertyOrThrow (O, P, desc)
 
-export function DefinePropertyOrThrow(realm: Realm, O: UnknownType, P: UnknownType, desc: UnknownType): Completion<UnknownType> {
+export function DefinePropertyOrThrow(realm: Realm, O: JSObject, P: JSPropertyKey, desc: PropertyDescriptor): Completion<boolean> {
     throw new Error("DefinePropertyOrThrow not implemented");
 }
 
 // ES6 Section 7.3.8: DeletePropertyOrThrow (O, P)
 
-export function DeletePropertyOrThrow(realm: Realm, O: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function DeletePropertyOrThrow(realm: Realm, O: JSObject, P: JSPropertyKey): Completion<boolean> {
     throw new Error("DeletePropertyOrThrow not implemented");
 }
 
 // ES6 Section 7.3.9: GetMethod (O, P)
 
-export function GetMethod(realm: Realm, O: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function GetMethod(realm: Realm, O: JSValue, P: JSPropertyKey): Completion<JSValue> {
     throw new Error("GetMethod not implemented");
 }
 
 // ES6 Section 7.3.10: HasProperty (O, P)
 
-export function HasProperty(realm: Realm, O: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function HasProperty(realm: Realm, O: JSObject, P: JSPropertyKey): Completion<boolean> {
     throw new Error("HasProperty not implemented");
 }
 
 // ES6 Section 7.3.11: HasOwnProperty (O, P)
 
-export function HasOwnProperty(realm: Realm, O: UnknownType, P: UnknownType): Completion<UnknownType> {
+export function HasOwnProperty(realm: Realm, O: JSObject, P: JSPropertyKey): Completion<boolean> {
     throw new Error("HasOwnProperty not implemented");
 }
 
 // ES6 Section 7.3.12: Call(F, V, [argumentsList])
 
-export function Call(realm: Realm, F: UnknownType, V: UnknownType, argumentList: UnknownType): Completion<UnknownType> {
+export function Call(realm: Realm, F: JSValue, V: JSValue, argumentList: JSValue[]): Completion<JSValue> {
     throw new Error("Call not implemented");
 }
 
 // ES6 Section 7.3.13: Construct (F, [argumentsList], [newTarget])
 
-export function Construct(realm: Realm, F: UnknownType, argumentList: UnknownType, newTarget: UnknownType): Completion<UnknownType> {
+export function Construct(realm: Realm, F: JSObject, argumentList: JSValue[], newTarget: JSObject): Completion<JSObject> {
     throw new Error("Construct not implemented");
 }
 
 // ES6 Section 7.3.14: SetIntegrityLevel (O, level)
 
-export function SetIntegrityLevel(realm: Realm, O: UnknownType, level: UnknownType): Completion<UnknownType> {
+export function SetIntegrityLevel(realm: Realm, O: JSObject, level: string): Completion<boolean> {
     throw new Error("SetIntegrityLevel not implemented");
 }
 
 // ES6 Section 7.3.15: TestIntegrityLevel (O, level)
 
-export function TestIntegrityLevel(realm: Realm, O: UnknownType, level: UnknownType): Completion<UnknownType> {
+export function TestIntegrityLevel(realm: Realm, O: JSObject, level: string): Completion<boolean> {
     throw new Error("TestIntegrityLevel not implemented");
 }
 
 // ES6 Section 7.3.16: CreateArrayFromList (elements)
 
-export function CreateArrayFromList(realm: Realm, elements: UnknownType): Completion<UnknownType> {
+export function CreateArrayFromList(realm: Realm, elements: JSValue[]): Completion<JSObject> {
     throw new Error("CreateArrayFromList not implemented");
 }
 
 // ES6 Section 7.3.17: CreateListFromArrayLike (obj [, elementTypes])
 
-export function CreateListFromArrayLike(realm: Realm, obj: UnknownType, elementTypes: UnknownType): Completion<UnknownType> {
+export function CreateListFromArrayLike(realm: Realm, obj: JSValue, elementTypes: any): Completion<JSValue[]> {
     throw new Error("CreateListFromArrayLike not implemented");
 }
 
 // ES6 Section 7.3.18: Invoke(O, P, [argumentsList])
 
-export function Invoke(realm: Realm, O: UnknownType, P: UnknownType, argumentsList: UnknownType): Completion<UnknownType> {
+export function Invoke(realm: Realm, O: JSObject, P: JSPropertyKey, argumentsList: JSValue[]): Completion<JSValue> {
     throw new Error("Invoke not implemented");
 }
 
 // ES6 Section 7.3.19: OrdinaryHasInstance (C, O)
 
-export function OrdinaryHasInstance(realm: Realm, C: UnknownType, O: UnknownType): Completion<UnknownType> {
+export function OrdinaryHasInstance(realm: Realm, C: any, O: JSObject): Completion<boolean> {
     throw new Error("OrdinaryHasInstance not implemented");
 }
 
 // ES6 Section 7.3.20: SpeciesConstructor (O, defaultConstructor)
 
-export function SpeciesConstructor(realm: Realm, O: UnknownType, defaultConstructor: UnknownType): Completion<UnknownType> {
+export function SpeciesConstructor(realm: Realm, O: JSObject, defaultConstructor: any): Completion<JSObject> {
     throw new Error("SpeciesConstructor not implemented");
 }
 
 // ES6 Section 7.3.21: EnumerableOwnNames (O)
 
-export function EnumerableOwnNames(realm: Realm, O: UnknownType): Completion<UnknownType> {
+export function EnumerableOwnNames(realm: Realm, O: JSObject): Completion<JSString[]> {
     throw new Error("EnumerableOwnNames not implemented");
 }
 
 // ES6 Section 7.3.22: GetFunctionRealm (obj)
 
-export function GetFunctionRealm(realm: Realm, obj: UnknownType): Completion<UnknownType> {
+export function GetFunctionRealm(realm: Realm, obj: JSObject): Completion<Realm> {
     throw new Error("GetFunctionRealm not implemented");
 }
