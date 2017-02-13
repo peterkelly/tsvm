4824ce9526108a8abfa80e01509ffd81bc67e1a2
Peter Kelly <peter@pmkelly.net>
2017-02-13 23:11:50 +0700

    While loops (partially complete)

---
+++ tests/execution/statements/While02.js
@@ -0,0 +1,20 @@
+console.log("Before loop");
+let i = null;
+while (i <= 5) {
+    console.log("i = "+i);
+    i++;
+}
+console.log("After loop");
+
+/*******************************************************************************
+execute
+
+Before loop
+i = null
+i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+After loop
+*******************************************************************************/
---
+++ tests/execution/statements/While03.js
@@ -0,0 +1,14 @@
+console.log("Before loop");
+let i = undefined;
+while (i <= 5) {
+    console.log("i = "+i);
+    i++;
+}
+console.log("After loop");
+
+/*******************************************************************************
+execute
+
+Before loop
+After loop
+*******************************************************************************/
