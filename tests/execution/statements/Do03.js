e7e6ed44adb648b14cb4f4c792be77e5b174e8b5
Peter Kelly <peter@pmkelly.net>
2017-02-14 17:57:44 +0700

    Do-while loops (partially complete)

---
+++ tests/execution/statements/Do03.js
@@ -0,0 +1,15 @@
+console.log("Before loop");
+let i = undefined;
+do {
+    console.log("i = "+i);
+    i++;
+} while (i <= 5);
+console.log("After loop");
+
+/*******************************************************************************
+execute
+
+Before loop
+i = undefined
+After loop
+*******************************************************************************/
