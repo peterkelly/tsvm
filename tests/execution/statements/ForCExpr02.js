07d38d2f70a5196fad028be5d1040dafb7e78c25
Peter Kelly <peter@pmkelly.net>
2017-02-14 23:12:32 +0700

    C-style for statements

---
+++ tests/execution/statements/ForCExpr02.js
@@ -0,0 +1,18 @@
+var i;
+console.log("Before loop");
+for (i = null; i <= 5; i++)
+    console.log("i = "+i);
+console.log("After loop");
+
+/*******************************************************************************
+execute
+
+Before loop
+i = null
+i = 1
+i = 2
+i = 3
+i = 4
+i = 5
+After loop
+*******************************************************************************/
---
+++ tests/execution/statements/ForCVar03.js
@@ -0,0 +1,11 @@
+console.log("Before loop");
+for (var i = undefined; i <= 5; i++)
+    console.log("i = "+i);
+console.log("After loop");
+
+/*******************************************************************************
+execute
+
+Before loop
+After loop
+*******************************************************************************/
