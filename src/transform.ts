8c741e7a91ebd20f8c6d790fc074827aed6f480a
Peter Kelly <peter@pmkelly.net>
2017-06-09 09:36:37 +0700

    tslint fixes

--- src/transform.ts
+++ src/transform.ts
@@ -1,3 +1,17 @@
+// Copyright 2016-2017 Peter Kelly <peter@pmkelly.net>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import * as grammar from "./parser/grammar";
 import {
     Grammar,
@@ -43,7 +57,6 @@ function expandFirstitem(g: Grammar): void {
 }
 
 function liftPrefix(g: Grammar): void {
-    let depth = 0;
     const visitor: grammar.Visitor = (action, visitChildren) => {
         action = visitChildren();
 
@@ -98,7 +111,7 @@ function printGrammar(g: Grammar): void {
         else if (action instanceof RefAction)
             console.log(padString(depth) + "[" + action.name + "]");
         else if (action instanceof SpliceNodeAction)
-            console.log(padString(depth) + "=> " + action.name + "(" + action.childIndices.join(",") + ")")
+            console.log(padString(depth) + "=> " + action.name + "(" + action.childIndices.join(",") + ")");
         else
             console.log(padString(depth) + (<any> action).constructor.name);
 
